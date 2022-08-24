a0e845882ee135591ad81a46e3545d6717849a33
WebSmaIL <smakovskiyy@yandex.ru>
2022-08-24 13:36:53 +0600

    install axios & add this library in package.json

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,43 +1,50 @@
+import axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
 
 const Users = (props) => {
     if (props.usersList.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                url: "https://coolsen.ru/wp-content/uploads/2021/06/15-8.jpg",
-                userName: "Jack N.",
-                userStatus: "I`m a boss",
-                location: {
-                    country: "USA",
-                    city: "New-York",
-                },
-                followed: true,
-            },
-            {
-                id: 2,
-                url: "https://pixelbox.ru/wp-content/uploads/2021/03/ava-instagram-1.jpeg",
-                userName: "Amin K.",
-                userStatus: "I`m a boss too",
-                location: {
-                    country: "Iran",
-                    city: "IranCity",
-                },
-                followed: false,
-            },
-            {
-                id: 3,
-                url: "https://pixelbox.ru/wp-content/uploads/2021/02/mult-ava-instagram-2.jpg",
-                userName: "Veronica G.",
-                userStatus: "and me too",
-                location: {
-                    country: "Russia",
-                    city: "Omsk",
-                },
-                followed: false,
-            },
-        ]);
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((res) => {
+                // debugger
+                props.setUsers(res.data.items);
+            });
+        // props.setUsers([
+        //     {
+        //         id: 1,
+        //         url: "https://coolsen.ru/wp-content/uploads/2021/06/15-8.jpg",
+        //         userName: "Jack N.",
+        //         userStatus: "I`m a boss",
+        //         location: {
+        //             country: "USA",
+        //             city: "New-York",
+        //         },
+        //         followed: true,
+        //     },
+        //     {
+        //         id: 2,
+        //         url: "https://pixelbox.ru/wp-content/uploads/2021/03/ava-instagram-1.jpeg",
+        //         userName: "Amin K.",
+        //         userStatus: "I`m a boss too",
+        //         location: {
+        //             country: "Iran",
+        //             city: "IranCity",
+        //         },
+        //         followed: false,
+        //     },
+        //     {
+        //         id: 3,
+        //         url: "https://pixelbox.ru/wp-content/uploads/2021/02/mult-ava-instagram-2.jpg",
+        //         userName: "Veronica G.",
+        //         userStatus: "and me too",
+        //         location: {
+        //             country: "Russia",
+        //             city: "Omsk",
+        //         },
+        //         followed: false,
+        //     },
+        // ]);
     }
     return (
         <div>
@@ -48,7 +55,11 @@ const Users = (props) => {
                             <div>
                                 <img
                                     className={s.userAvatar}
-                                    src={user.url}
+                                    src={
+                                        user.photos.small != null
+                                            ? user.photos.small
+                                            : "https://startupfellows.ru/img/male.png"
+                                    }
                                     alt=""
                                 />
                             </div>
@@ -75,13 +86,21 @@ const Users = (props) => {
                             </div>
                         </div>
                         <div className={s.userInfo}>
-                            <span>{user.userName}</span>
-                            <span>{user.userStatus}</span>
+                            <span>
+                                <b>{user.name}</b>
+                            </span>
+                            <span>
+                                <i>
+                                    {user.status != null
+                                        ? user.status
+                                        : "This user don`t have status"}
+                                </i>
+                            </span>
                         </div>
-                        <div className={s.userLocation}>
+                        {/* <div className={s.userLocation}>
                             <span>{user.location.country}</span>
                             <span>{user.location.city}</span>
-                        </div>
+                        </div> */}
                     </div>
                 );
             })}
